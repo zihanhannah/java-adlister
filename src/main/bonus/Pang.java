77266b1c66a589a217a7e6347cf745002523374d
zihan zhao <zihanhannah@gmail.com>
2020-09-14 16:48:45 -0500

    exercise

--- src/main/bonus/Pang.java
+++ src/main/bonus/Pang.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "PangServlet", urlPatterns = "/pang")
+//4. Create a servlet that listens for requests to ```/ping``` and a servlet that listens for requests to ```/pong```. The former servlet should simply return a link that sends a request to the latter, and vice versa! If successfully built, the user should be able to cycle back and forth between two pages by clicking on a ```PING!``` or ```PONG!``` link.
 public class Pang extends HttpServlet {
         public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
             res.setContentType("text/html");
