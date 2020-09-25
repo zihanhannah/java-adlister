2b577e33052c0d2d8449630d72d9336458a8ecf6
zihan zhao <zihanhannah@gmail.com>
2020-09-25 08:23:15 -0500

    m

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,11 +5,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello/")
 public class HelloWorldServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
 //        response.setContentType("text/html");
         String name = request.getParameter("name");
         if(name == null){
