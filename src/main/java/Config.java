057448ef38a78a163fce7bc1868268c18d978991
zihan zhao <zihanhannah@gmail.com>
2020-09-28 23:08:08 -0500

    exercise

---
+++ src/main/java/Config.java
@@ -0,0 +1,14 @@
+class Config {
+    public Config() {
+    }
+
+    public String getUrl() {
+        return "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";
+    }
+    public String getUser() {
+        return "root";
+    }
+    public String getPassword() {
+        return "root";
+    }
+}
