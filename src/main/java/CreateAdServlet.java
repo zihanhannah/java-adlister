3a03007ba9a90afdd89f7b7da900ff16f12b8f28
zihan zhao <zihanhannah@gmail.com>
2020-09-28 19:09:38 -0500

    exercise

--- src/main/java/CreateAdServlet.java
+++ src/main/java/CreateAdServlet.java
@@ -1,24 +1,29 @@
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//@WebServlet(name = "CreateAdServlet", urlPatterns = "/ads/create")
-//public class CreateAdServlet extends HttpServlet {
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
-//            .forward(request, response);
-//    }
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        Ad ad = new Ad(
-//            1, // for now we'll hardcode the user id
-//            request.getParameter("title"),
-//            request.getParameter("description")
-//        );
-//        DaoFactory.getAdsDao().insert(ad);
-//        response.sendRedirect("/ads");
-//    }
-//}
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "CreateAdServlet", urlPatterns = "/ads/create")
+public class CreateAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+            .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Ad ad = new Ad(
+            1, // for now we'll hardcode the user id
+            request.getParameter("title"),
+            request.getParameter("description")
+        );
+        try {
+            DaoFactory.getAdsDao().insert(ad);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        response.sendRedirect("/ads");
+    }
+}
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,7 +1,7 @@
 import com.mysql.jdbc.Driver;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads {
@@ -15,12 +15,23 @@ public class MySQLAdsDao implements Ads {
     }
 
     @Override
-    public List<Ad> all() {
-        return null;
+    public List<Ad> all() throws SQLException {
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+        List<Ad> ads = new ArrayList<Ad>();
+        while(rs.next()){//when loop all the ads, rs.next() will return null
+            Ad ad = new Ad(rs.getLong(1),rs.getLong(2),rs.getString(3),rs.getString(4));
+            ads.add(ad);
+        }
+        return ads;
     }
 
     @Override
-    public Long insert(Ad ad) {
-        return null;
+    public Long insert(Ad ad) throws SQLException {
+        Statement stmt = connection.createStatement();//execute query in mySQL
+        stmt.executeUpdate(String.format("INSERT INTO ads(user_id, title, description) VALUES(%d, '%s', '%s')", ad.getUserId(), ad.getTitle(), ad.getDescription()),Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = stmt.getGeneratedKeys();//a set of new ads
+        rs.next();//go to the first ad we created
+        return rs.getLong(1);//get the first column in the database. or getString(num);
     }
 }
